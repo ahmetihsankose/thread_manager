f1fdb4ed2bc42f1837bde804b2412d84a751bf4b
ahmetihsankose <ahmetihsankose.61@gmail.com>
2023-04-30 14:22:26 +0300

    test synchronous added

--- test/TestModule.h
+++ test/TestModule.h
@@ -1,13 +1,43 @@
 #pragma once
 #include <iostream>
+#include <pthread.h>
 
-class TestModule
+extern sem_t sem1, sem2, sem3, sem4;
+
+class TestModule1
 {
 public:
     void tick()
     {
+        std::cout << "ticked 1" << std::endl;
+        sem_post(&sem1);
+        sem_wait(&sem4);
+        std::cout << "ticked 5" << std::endl;
     }
+};
 
-private:
-    int counter = 0;
+class TestModule2
+{
+public:
+    void tick()
+    {
+        sem_wait(&sem1);
+        std::cout << "ticked 2" << std::endl;
+        sem_post(&sem2);
+
+        sem_wait(&sem3);
+        std::cout << "ticked 4" << std::endl;
+        sem_post(&sem4);
+    }
+};
+
+class TestModule3
+{
+public:
+    void tick()
+    {
+        sem_wait(&sem2);
+        std::cout << "ticked 3" << std::endl;
+        sem_post(&sem3);
+    }
 };
--- test/main.cpp
+++ test/main.cpp
@@ -5,15 +5,16 @@
 #include "ThreadManager.h"
 #include "WorkerThread.h"
 #include "TestModule.h"
-#include "TestModule1.h"
-#include "TestModule2.h"
 #include "Logger.h"
 #include "Exception.h"
+#include <semaphore.h>
+
+sem_t sem1, sem2, sem3, sem4;
 
 ThreadManager threadManager;
-std::shared_ptr<TestModule> testModule = std::make_shared<TestModule>();
 std::shared_ptr<TestModule1> testModule1 = std::make_shared<TestModule1>();
 std::shared_ptr<TestModule2> testModule2 = std::make_shared<TestModule2>();
+std::shared_ptr<TestModule3> testModule3 = std::make_shared<TestModule3>();
 
 int main()
 {
@@ -21,22 +22,28 @@ int main()
     {
         LOG_FILE("/home/kose/workspace/modular-threads-manager/logfile.txt");
 
-        threadManager.createThread<WorkerThread>(0, 45, 1e9, 0, testModule, [](std::shared_ptr<void> obj)
-                                                 { std::static_pointer_cast<TestModule>(obj)->tick(); });
+        sem_init(&sem1, 0, 0);
+        sem_init(&sem2, 0, 0);
+        sem_init(&sem3, 0, 0);
+        sem_init(&sem4, 0, 0);
 
-        threadManager.createThread<WorkerThread>(1, 45, 1e9, 0, testModule1, [](std::shared_ptr<void> obj)
-                                                 { std::static_pointer_cast<TestModule>(obj)->tick(); });
+        threadManager.createThread<WorkerThread>(0, 45, 1e9, 0, testModule1, [](std::shared_ptr<void> obj)
+                                                 { std::static_pointer_cast<TestModule1>(obj)->tick(); });
 
-        threadManager.createThread<WorkerThread>(2, 45, 1e9, 0, testModule2, [](std::shared_ptr<void> obj)
-                                                 { std::static_pointer_cast<TestModule>(obj)->tick(); });
+        threadManager.createThread<WorkerThread>(1, 45, 1e9, 0, testModule2, [](std::shared_ptr<void> obj)
+                                                 { std::static_pointer_cast<TestModule2>(obj)->tick(); });
 
-        threadManager.startAllThreads();
+        threadManager.createThread<WorkerThread>(2, 45, 1e9, 0, testModule3, [](std::shared_ptr<void> obj)
+                                                 { std::static_pointer_cast<TestModule3>(obj)->tick(); });
+
+
         threadManager.setRecordStats(0, true);
         threadManager.setRecordStats(1, true);
         threadManager.setRecordStats(2, true);
         threadManager.writeAllThreadStatsToFile("/home/kose/workspace/modular-threads-manager/test.txt");
-        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-        
+
+        threadManager.startAllThreads();
+
     }
     catch (const Exception &e)
     {
